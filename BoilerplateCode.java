d4219831a276ca70ab4f289bfde007ffe0e99111
Utkarsh Dubey <304011684+utkarsh-dubeyy@users.noreply.github.com>
2026-09-14 19:35:37 +0530

    Day 1: First Java programs

---
+++ BoilerplateCode.java
@@ -0,0 +1,5 @@
+public class BoilerplateCode {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ Introduction.java
@@ -0,0 +1,6 @@
+public class Introduction {
+    public static void main(String args[]) {
+        System.out.println("Hello, I am Utkarsh Dubey.");
+        System.out.println("It's Day 1 of my Java Learning.");
+    }
+}
