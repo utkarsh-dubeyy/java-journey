50176ad027dc06ecbf1f7aeb68a835e3d7372b65
Utkarsh Dubey <304011684+utkarsh-dubeyy@users.noreply.github.com>
2026-09-17 20:29:06 +0530

    Fix formatting and text

--- StarPattern.java
+++ StarPattern.java
@@ -1,5 +1,5 @@
 public class StarPattern {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("****");
         System.out.println("***");
         System.out.println("**");
